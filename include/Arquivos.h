54d26f3036b5d44ac7289c564f89d4c18b804b35
jpkbraun <108730828+jpkbraun@users.noreply.github.com>
2022-09-08 18:32:23 -0300

    Add files via upload
    B-Tree e TRIE funcionando

--- include/Arquivos.h
+++ include/Arquivos.h
@@ -5,8 +5,13 @@
 #include <sstream>
 #include <utility>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX 3 // Define o grau das árvores B+
+#define NTRIE 26
+#define ANIME 1
+#define MANGA 2
 
 short AbreArquivo(FILE **arq, char nome_arq[], char *modo);
 short ArquivoExiste(std::string name);
@@ -40,3 +45,33 @@ class BPTree{
         void removeInterno(int chave, Nodo *cursor, Nodo *filho);
         Nodo *achaPai(Nodo *cursor, Nodo *filho);
 };
+
+typedef struct NodoTrie NodoTrie;
+
+struct NodoTrie {
+    char caracter;
+    NodoTrie* filhos[NTRIE];
+    int eh_folha;
+    int indexanime;
+    int indexmanga;
+};
+
+NodoTrie* cria_NodoTrie(char caracter);
+
+void free_NodoTrie(NodoTrie* nodo);
+
+NodoTrie* insert_trie(NodoTrie* raiz, std::string name, int index, int anime_ou_manga);
+
+int busca_trie(NodoTrie* raiz, std::string name, int anime_ou_manga);
+
+int checa_divergencia(NodoTrie* raiz, std::string name);
+
+std::string busca_maior_prefixo(NodoTrie* raiz, std::string name);
+
+int nodo_eh_folha(NodoTrie* raiz, std::string name);
+
+NodoTrie* delete_trie(NodoTrie* raiz, std::string name);
+
+void print_trie(NodoTrie* raiz);
+
+void print_nome(NodoTrie* raiz, std::string name, int anime_ou_manga);
