355d25cbcf925387581c4dcfd68a4ea7d3c36995
jpkbraun <108730828+jpkbraun@users.noreply.github.com>
2022-09-16 01:15:39 -0300

    add busca dois mesmo campo

--- include/Operacoes.h
+++ include/Operacoes.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #define GENRES 0
 #define LICENSORS 1
+#define STUDIOS 2
 
 void buscaAnimePorPrefixo(char* nome);
 void buscaMangaPorPrefixo(char* nome);
@@ -20,3 +21,5 @@ std::vector<int> geraSequenciaDeGaps(int tam);
 trie_string* cria_arq_inv(trie_string* raiz, int tipo);
 
 void Busca_Dois_Campos(char* nome1, char* nome2, trie_string* raiz1, trie_string* raiz2);
+
+void Busca_Dois_Mesmo_Campo(char* nome1, char* nome2, trie_string* raiz1);
